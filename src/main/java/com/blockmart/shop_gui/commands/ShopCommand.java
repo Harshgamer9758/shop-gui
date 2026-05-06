27c7e3081fdbdcd1298a577e56703ca5ea25a4e8
Harshgamer9758 <jadhaokishor08@gmail.com>
2026-05-06 22:22:47 +0530

    feat: add src/main/java/com/blockmart/shop_gui/commands/ShopCommand.java

---
+++ src/main/java/com/blockmart/shop_gui/commands/ShopCommand.java
@@ -0,0 +1,28 @@
+package com.blockmart.shop_gui.commands;
+
+import com.blockmart.shop_gui.managers.ShopManager;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class ShopCommand implements CommandExecutor {
+
+    private final ShopManager shopManager;
+
+    public ShopCommand(ShopManager shopManager) {
+        this.shopManager = shopManager;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("§cOnly players can use this command.");
+            return true;
+        }
+
+        Player player = (Player) sender;
+        shopManager.openShopGUI(player, 1);
+        return true;
+    }
+}
